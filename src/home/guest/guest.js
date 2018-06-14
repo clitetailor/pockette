7b0e6079fdaabc2ed3c6eac1ba69a5e8c36cccb4
Đặng Hiếu <hieudm@coccoc.com>
2018-06-14 21:14:57 +0700

    test layout

---
+++ src/home/guest/guest.js
@@ -0,0 +1,10 @@
+import template from './guest.pug'
+
+export const GuestComponent = {
+    template: template(),
+    controller: GuestController
+}
+
+export function GuestController() {
+
+}
--- src/home/home.js
+++ src/home/home.js
@@ -8,5 +8,30 @@ export const HomeComponent = {
 export function HomeController($scope, $route) {
   this.$onInit = function () {
     $scope.title = 'Pockette'
+    $scope.balance = 0
+    $scope.addValue = 0
+    $scope.list = []
+    $scope.expenditureName = undefined
+    $scope.expenditureAmount = 0
+    $scope.list=[
+      {
+        name: 'laptop',
+        amount: '$1327'
+      }
+    ]
+
+    $scope.addBalance = function () {
+      $scope.balance += $scope.addValue
+      $scope.addValue = 0
+    }
+    $scope.addExpenditure = function () {
+      if($scope.expenditureName){
+        let newExpenditure = {
+          name: $scope.expenditureName,
+          amount: $scope.expenditureAmount
+        }
+        $scope.list.push(newExpenditure)
+      }
+    }
   }
 }
